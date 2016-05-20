2dabedd067ffa290b45c9093b53289d768d16497
ChauKhacHai <khachai.itcs@gmail.com>
2016-05-20 13:39:41 +0700

    First  Command

---
+++ DAO/HocVienDAO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+using DTO;
+namespace DAO
+{
+    public class HocVienDAO : DBConnection
+    {
+        public HocVienDAO() : base() { }
+
+        DataTable dt = new DataTable();
+        //public DataTable DanhSachMaHocVien()
+        //{
+
+        //    try
+        //    {
+        //        if (conn.State != ConnectionState.Open)
+        //            conn.Open();
+
+        //        SqlCommand cmd = conn.CreateCommand();
+        //        cmd.CommandText = string.Format("SELECT * FORM HocVien", conn);
+        //        try
+        //        {
+        //            SqlDataReader rd = cmd.ExecuteReader();
+        //            dt.Load(rd);
+        //            return dt;
+        //        }
+        //        catch
+        //        {
+        //            conn.Close();
+        //            throw;
+        //        }
+        //    }
+        //}
+
+    }
+}
